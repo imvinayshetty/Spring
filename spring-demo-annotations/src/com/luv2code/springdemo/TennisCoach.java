30a6d1a715ca93c11a4dca06f8e9a1f46bfc4182
Vinay Shetty <vinay.shetty@capgemini.com>
2020-06-06 12:19:12 +0530

    Changes till 5th June 2020

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,14 +1,28 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("thatSillycoach")
 public class TennisCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		this.fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		
 		return "Practice my backhand";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
 	}
 
 }
